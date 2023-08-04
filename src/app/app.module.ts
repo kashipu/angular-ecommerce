c6220eb29a52946c928208b96af8b0556f71a50c
William Moreno <graficowm@gmail.co>
2023-08-03 22:05:18 -0500

    Header and top alert done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TopAlertComponent } from './components/top-alert/top-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TopAlertComponent
   ],
   imports: [
     BrowserModule,
