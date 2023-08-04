8335f1003a3b9c8323c55176322da3264d07e34b
William Moreno <graficowm@gmail.co>
2023-08-04 00:17:17 -0500

    Fix grid product

--- src/app/pages/zapatos-hombre/zapatos-hombre.component.ts
+++ src/app/pages/zapatos-hombre/zapatos-hombre.component.ts
@@ -8,6 +8,7 @@ import { Title } from '@angular/platform-browser';
 })
 export class ZapatosHombreComponent {
   titlePage = 'Adidas | Zapatos para Hombre';
+  cantProducts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   constructor(private titleService: Title) {
     this.titleService.setTitle(this.titlePage);
   }
