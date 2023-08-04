688163d0c13190b5c29616d0d216d7e59e0e4a6f
William Moreno <graficowm@gmail.co>
2023-08-03 22:57:29 -0500

    Add(+) Card product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TopAlertComponent } from './components/top-alert/top-alert.component';
+import { BreadcrumbComponent } from './components/breadcrumb/breadcrumb.component';
+import { PageTitleComponent } from './components/page-title/page-title.component';
+import { CardProductComponent } from './components/card-product/card-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TopAlertComponent
+    TopAlertComponent,
+    BreadcrumbComponent,
+    PageTitleComponent,
+    CardProductComponent
   ],
   imports: [
     BrowserModule,
